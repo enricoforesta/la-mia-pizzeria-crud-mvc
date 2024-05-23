4a40957df5a9b473d6b0903df533e80b15a97959
enricoforesta <enrico.foresta97@gmail.com>
2024-05-23 14:30:41 +0200

    feat: aggiunta homepage

--- la-mia-pizzeria-static/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ la-mia-pizzeria-static/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -80,7 +80,7 @@ namespace la_mia_pizzeria_static.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Ricorda")]
             public bool RememberMe { get; set; }
         }
 
@@ -103,7 +103,7 @@ namespace la_mia_pizzeria_static.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/Pizza/");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
--- la-mia-pizzeria-static/Program.cs
+++ la-mia-pizzeria-static/Program.cs
@@ -44,7 +44,7 @@ namespace la_mia_pizzeria_static
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Pizza}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Index}/{id?}");
             app.MapRazorPages();
 
             app.Run();
